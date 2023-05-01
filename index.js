f5810e3109077eec1150b89d9356802a181b8e65
annashamaeva <shamaevanna@gmail.com>
2023-05-01 17:22:32 +0200

    build

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {request, response, query} = require("express");
 const app = express()
 const cors=require("cors")
+const morgan = require('morgan')
 require('dotenv').config()
 
 const Person = require('./models/person')
